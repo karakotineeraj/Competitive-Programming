6ae0b0d99b4d77a9a1937af0251bd8262b443b87
karakotineeraj <neerajkarakoti13@gmail.com>
2022-02-10 19:14:53 +0530

    Empty CPP File

---
+++ cp.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+#define vi vector<int>
+#define vb vector<bool>
+#define vll vector<long long>
+ 
+#define pi pair<int,int>
+#define pl pair<long,long>
+ 
+#define ll long long
+ 
+#define M 1000000007
+ 
+using namespace std;
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("error.txt", "w", stderr);
+        freopen("output.txt", "w", stdout);
+    #endif
+ 
+
+ 
+    cerr<<"time taken: "<<(float)clock()/CLOCKS_PER_SEC<<" secs\n";
+ 
+    #ifndef ONLINE_JUDGE
+        fclose(stdout);
+        fclose(stderr);
+        fclose(stdin);
+    #endif
+ 
+    return 0;
+}
