d6fbb6d66a66c690887fb39e819b8ae52b657ce8
karakotineeraj <neerajkarakoti13@gmail.com>
2022-02-15 15:09:12 +0530

    H-Index

--- cp.cpp
+++ cp.cpp
@@ -1,31 +1,47 @@
-#include <iostream>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-char vowels[] = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+// Keep a MIN Heap and insert the latest element while traversing the array.
+// While the minimum element of the heap < heap size,
+// keep popping the top until the top element >= heap size.
 
-string GetRuler(const string& kingdom) {
-    if(kingdom[kingdom.size() - 1] == 'y' || kingdom[kingdom.size() - 1] == 'Y')
-        return "nobody";
+vector<int> GetHIndexScore(vector<int> citations_per_paper) {
+    int n = citations_per_paper.size();
 
-    for(int i=0; i<10; ++i) {
-        if(kingdom[kingdom.size() - 1] == vowels[i])
-            return "Alice";
+    vector<int> ans(n);
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for(int i=0; i<n; ++i) {
+        pq.push(citations_per_paper[i]);
+
+        while(pq.top() < pq.size())
+            pq.pop();
+
+        ans[i] = pq.size();
     }
 
-    return "Bob";
+    return ans;
 }
 
 int main() {
-  int testcases;
-  cin >> testcases;
-  string kingdom;
+    int tests;
+    cin >> tests;
+    for (int test_case = 1; test_case <= tests; test_case++) {
+        int paper_count;
+        cin >> paper_count;
 
-  for (int t = 1; t <= testcases; ++t) {
-    cin >> kingdom;
-    cout << "Case #" << t << ": " << kingdom << " is ruled by "
-         << GetRuler(kingdom) << ".\n";
-  }
-  return 0;
+        vector<int> citations(paper_count);
+        for (int i = 0; i < paper_count; i++) {
+          cin >> citations[i];
+        }
+
+        vector<int> answer = GetHIndexScore(citations);
+        cout << "Case #" << test_case << ": ";
+        
+        for (int i = 0; i < answer.size(); i++) {
+          cout << answer[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
