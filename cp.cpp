4b799acb5d6731273425f0ddd7481a67ad58fc58
karakotineeraj <neerajkarakoti13@gmail.com>
2022-02-24 13:12:47 +0530

    Exponentiation

--- cp.cpp
+++ cp.cpp
@@ -12,6 +12,24 @@
 #define M 1000000007
  
 using namespace std;
+
+// We can write a ^ b = multiply(a^x)
+// where x = 2^(postion of set bits)
+// b -> sum(b(i) * 2^i)
+ll binaryModExp(ll a, ll b, ll m) {
+    ll ans = 1;
+
+    a = a % m;
+    while(b) {
+        if(b % 2) 
+            ans = (ans * a) % m;
+
+        a = (a * a) % m;
+        b /= 2;
+    }
+
+    return ans % m;
+}
  
 int main() {
     ios_base::sync_with_stdio(false);
@@ -23,7 +41,14 @@ int main() {
         freopen("output.txt", "w", stdout);
     #endif
  
+    int n, a, b;
+    cin>>n;
 
+    while(n--) {
+        cin>>a>>b;
+
+        cout<<binaryModExp(a, b, M)<<endl;
+    }
  
     cerr<<"time taken: "<<(float)clock()/CLOCKS_PER_SEC<<" secs\n";
  
