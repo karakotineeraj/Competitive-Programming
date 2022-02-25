7a5ca826b0486fd58a90cc31c23792944d1632a4
karakotineeraj <neerajkarakoti13@gmail.com>
2022-02-25 11:27:20 +0530

    Point Location Test

--- cp.cpp
+++ cp.cpp
@@ -12,6 +12,15 @@
 #define M 1000000007
  
 using namespace std;
+
+// Create two vectors :-
+// (i) (x1, y1) -> (x2, y2)
+// (ii) (x1, y1) -> (x3, y3)
+// 
+// Now take the Cross Product of the vectors from P2 to P3.
+// If it is 0, then it is on the line 
+// and if it is positive then P3 is on Left else it is on right.
+// The formula was derived from the Cross Product. 
  
 int main() {
     ios_base::sync_with_stdio(false);
@@ -23,7 +32,23 @@ int main() {
         freopen("output.txt", "w", stdout);
     #endif
  
+    int t;
+    cin>>t;
 
+    ll x1, x2, x3, y1, y2, y3;
+    while(t--) {
+        cin>>x1>>y1>>x2>>y2>>x3>>y3;
+        ll direction = x2*y3 - x2*y1 - x1*y3 - x3*y2 + x1*y2 + x3*y1;
+
+        // cout<<direction<<endl;
+        if(direction == 0)
+            cout<<"TOUCH";
+        else if(direction < 0)
+            cout<<"RIGHT";
+        else cout<<"LEFT";
+
+        cout<<"\n";
+    }
  
     cerr<<"time taken: "<<(float)clock()/CLOCKS_PER_SEC<<" secs\n";
  
